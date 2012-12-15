e25838de13b8eb30cef26e0b7bbfa2d9c4ed41e5
Hung Dao <mephis1987@gmail.com>
2012-12-14 17:36:00 -0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,2 +1,6 @@
 # San Francisco WOEID is 12797153
+# Helsinki WOEID is 565346
+# Ho Chi Minh WOEID is 1252431
+# Find your city WOEID at http://woeid.rosselliot.co.nz/
+
 curl --silent "http://weather.yahooapis.com/forecastrss?w=12797153&u=c" | grep -E '(Current Conditions:|C<BR)' | tail -n1 | sed -e 's/<BR \/>//' -e 's/\(.*\), \(.*\) C$/\2˚C/'
